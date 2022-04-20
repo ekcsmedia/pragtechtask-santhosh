454885d2757b036069cc8993609d0c756edd683b
Santhosh Kumar S <79499174+ekcsmedia@users.noreply.github.com>
2022-04-20 17:19:01 +0530

    Update sharedPrefs.dart

--- sharedPrefs.dart
+++ sharedPrefs.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:pragtechtask/model/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
+// to convert the objects list to string and use it in shared preference and get back the objects from String
 
 Future<List<UserModel>> sharedpref(var listdata) async {
   List<UserModel> _likedList = listdata;
